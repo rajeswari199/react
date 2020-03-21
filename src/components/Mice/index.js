d5714d8c6df930cd1d22e8a3477d9754b605f414
rajeswari <raji.thavamuthu1196@gmail.com>
2020-03-21 13:02:02 +0530

    added comments

--- src/components/Mice/index.js
+++ src/components/Mice/index.js
@@ -7,25 +7,42 @@ class Mice extends React.Component {
         super();
         this.state = {
             path: [], array: [
-                [1, 0, 1, 0],
-                [1, 0, 0, 1],
-                [1, 1, 1, 1]
+                [1, 1, 0, 1],
+                [0, 1, 0, 1],
+                [0, 1, 1, 1]
             ]
         }
     }
 
     onClick = (event) => {
         event.preventDefault();
-        const path = this.state.array[2][3] !== 0 ? 
-        this.calculate([{ i: 0, j: 0 }], 0, 0, [])
-        : 'not reachable'
+        const arr = this.state.array
+        const path = this.state.array[arr.length - 1][arr[0].length - 1] !== 0 && arr[0][0] !== 0 ?
+            this.calculate([{ i: 0, j: 0 }], 0, 0, [])
+            : this.setState({ path: 'not reachable' })
     };
 
-    omitFunction = (i,j,omit) => this.state.array[i][j] && _.findIndex(omit, {i: i,j:j}) === -1
+    /**
+     * return false 
+     * if i or j value exceeds the array length (to omit out of bound index) 
+     * index has false value i.e 0
+     * if the index is found in omit array
+     */
+    omitFunction = (i, j, omit) =>
+        i !== this.state.array.length
+        && j !== this.state.array[0].length
+        && this.state.array[i][j]
+        && _.findIndex(omit, { i: i, j: j }) === -1
 
-    calculate(path, i, j,omit) {
-        const arrLength = this.state.array[0].length;
-        let pushObj = this.omitFunction(i,j+1,omit)
+    calculate(path, i, j, omit) {
+        const arr = this.state.array;
+        let pushObj = {}
+        /**
+         * assign pushObject with immediate right indices (i,j+1) if its value is 1 
+         * else with immediate below indices (i+1,j) if its value is 1
+         * else with empty object if either ways are closed i.e, 0
+         */
+        pushObj = this.omitFunction(i, j + 1, omit)
             ? { i: i, j: j + 1 }
             : this.omitFunction(i + 1, j, omit)
                 ? { i: i + 1, j: j }
@@ -33,26 +50,49 @@ class Mice extends React.Component {
         if (!_.isEmpty(pushObj)) {
             path.push(pushObj)
         } else {
+            /**
+             * backtrack by popping out the recently pushed value if pushObject is empty
+             * this is because the path becomes unreachable so go with other option
+             */
             path.pop()
-            if(!path.length){
+            if (!path.length) {
+                /**
+                 * array does not have any reachable path, if initial value (0,0) is popped out 
+                 * and 'path' becomes an empty array.
+                 */
                 console.log('not reachable')
                 return
             }
+            /**
+             * assign pushObject with the last value of 'path', to calculate the alternate path.
+             * push current location to 'omit' array, to avoid going to same location again.
+             */
             pushObj = { ...path[path.length - 1] }
             omit.push({ i: i, j: j })
         }
-        if (pushObj.i === this.state.array.length - 1 && pushObj.j === arrLength - 1) {
-            console.log(path)
+        /**
+         * setstate if the destination is reached.
+         */
+        if (pushObj.i === arr.length - 1 && pushObj.j === arr[0].length - 1) {
+            this.setState({
+                path: path
+            })
             return [...path]
         }
-        this.calculate(path, pushObj.i, pushObj.j,omit)
+        this.calculate(path, pushObj.i, pushObj.j, omit)
     }
 
     render() {
         return (
             <div>
-                {this.state.path && <h2>path = {this.state.path}</h2>}
-                {this.state.array}
+                {this.state.path && <h2>path = {JSON.stringify(this.state.path)}</h2>}
+                {<table>
+                    <tbody>
+                        {_.map(this.state.array, (arr, arrIndex) => <tr key={arrIndex}>
+                            {_.map(arr, (val, index) => <td key={arrIndex + '-' + index}><h4>{val}</h4></td>)}
+                        </tr>)}
+                    </tbody>
+                </table>}
                 <button onClick={this.onClick}>Find Path</button>
             </div >
         )
